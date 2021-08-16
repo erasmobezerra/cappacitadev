fe8370f2d7a47ef0aad9036ef3876a88b9dbb86c
Erasmo Bezerra <erasmo.ads.tech@gmail.com>
2021-08-16 17:54:38 -0300

    corrigindo

--- Fase-ToHero/modulo.js
+++ Fase-ToHero/modulo.js
@@ -1,3 +1,5 @@
+// Como tornar visível variáveis em outros arquivos/módulos
+
 const saudacao = "Oi, tudo bem?"
 
 module.exports = {
--- Fase-ToHero/numerosAleatorios.js
+++ Fase-ToHero/numerosAleatorios.js
@@ -1,3 +1,11 @@
+// Pacote "lodash" => sudo npm install lodash
+
+// É uma boa prática nomear como anderline quando chamamos a biblioteca lodash
+// //const _ = require('lodash')
+
+// A função random() do pacote lodash imprime um número aleatoŕio entre um min e um max passados como argumentos.
+// //console.log(_.random(1, 100))
+
 const _ = require('lodash')
 
 console.log(_.random(1, 100))
